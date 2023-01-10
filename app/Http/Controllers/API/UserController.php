264030d1855a6dc4961d1b6c7ed2c8efaee38cce
unknown <haidaomar663@gmail.com>
2023-01-10 14:16:06 +0100

    [add: ImageService for upload image]

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,90 +3,50 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use App\Http\Requests\User\UpdateRequest;
-use Illuminate\Http\Request;
+use App\Models\User;
+use App\Services\ImageService;
 
 class UserController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  int  $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function show(int $id)
     {
         try {
-
             $user = User::findOrFail($id);
 
-            return response()->json([
-                'user' => $user
-            ], 200);
+            return response()->json(['user' => $user], 200);
 
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             return response()->json([
-                'error' => 'Something went wrong in UserController.show',
-                'message' => $e.getMessage()
+                'message' => 'Something went wrong in UserController.show',
+                'error' => $e->getMessage()
             ], 400);
         }
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\User\UpdateRequest  $request
      * @param  int  $id
+     *
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateRequest $request, $id)
+    public function update(UpdateRequest $request, int $id)
     {
         try {
-
             $user = User::findOrFail($id);
+
+            if ($request->hasFile('image')) {
+                (new ImageService)->updateImage($user, $request, '/images/users/', 'update');
+            }
 
             $user->first_name = $request->first_name;
             $user->last_name = $request->last_name;
@@ -97,22 +57,11 @@ class UserController extends Controller
 
             return response()->json('User details updated', 200);
 
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             return response()->json([
-                'error' => 'Something went wrong in UserController.update',
-                'message' => $e.getMessage()
+                'message' => 'Something went wrong in UserController.update',
+                'error' => $e->getMessage()
             ], 400);
         }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
